65e158a73140fa64fb198d8d7af013a4aff74fae
yanjixian <315644588@qq.com>
2021-10-13 17:13:15 +0800

    lession 14

--- MountainAirport/FlightStatusBoard/FlightList.swift
+++ MountainAirport/FlightStatusBoard/FlightList.swift
@@ -30,16 +30,34 @@ import SwiftUI
 
 struct FlightList: View {
   var flights: [FlightInformation]
-
+    var nextFlightId: Int {
+      guard let flight = flights.first(
+        where: {
+            $0.localTime >= Date()
+        }
+      ) else {
+        return flights.last!.id
+      }
+      return flight.id
+    }
   var body: some View {
-      ScrollView([.vertical], showsIndicators: false) {
-          VStack(alignment: .leading) {
-            ForEach(flights) { flight in
+      ScrollViewReader { scrollProxy in
+
+           List(flights) { flight in
               NavigationLink(destination: FlightDetails(flight: flight)) {
                 FlightRow(flight: flight)
               }
             }
+      .onAppear {
+          // 1
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            // 2
+              withAnimation {
+
+              scrollProxy.scrollTo(nextFlightId, anchor: .center)
+              }
           }
+        }
       }
   }
 }
--- MountainAirport/SearchFlights/SearchFlights.swift
+++ MountainAirport/SearchFlights/SearchFlights.swift
@@ -48,7 +48,19 @@ struct SearchFlights: View {
 
     return matchingFlights
   }
-
+    
+    var flightDates: [Date] {
+      let allDates = matchingFlights.map { $0.localTime.dateOnly }
+      let uniqueDates = Array(Set(allDates)) // 去重
+      return uniqueDates.sorted()
+    }
+    
+    func flightsForDay(date: Date) -> [FlightInformation] {
+      matchingFlights.filter {
+        Calendar.current.isDate($0.localTime, inSameDayAs: date)
+      }
+    }
+    
   var body: some View {
     ZStack {
       Image("background-view")
@@ -66,7 +78,23 @@ struct SearchFlights: View {
         .pickerStyle(SegmentedPickerStyle())
         TextField(" Search cities", text: $city)
           .textFieldStyle(RoundedBorderTextFieldStyle())
-        // Insert Results
+        
+          List{
+              ForEach(flightDates, id: \.hashValue) { date in
+                  Section(header: Text(longDateFormatter.string(from: date)),
+                          footer: HStack {
+                      Spacer()
+                      Text("Matching flights " +
+                                      "\(flightsForDay(date: date).count)")
+                  }) {
+                      ForEach(flightsForDay(date: date)) { flight in
+                              SearchResultRow(flight: flight)
+                            }
+                  }
+                  
+              }
+              
+          }.listStyle(.insetGrouped)
         Spacer()
       }.navigationBarTitle("Search Flights")
       .padding()
